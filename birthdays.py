8a89ba500e17a1d840f0a4f037e3ff4d8ffe97af
Lucek_work <lkm@o2.pl>
2017-12-06 13:46:50 +0100

    chapter 5

---
+++ birthdays.py
@@ -0,0 +1,16 @@
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+while True:
+    name = input('Enter a name: blank to quit) ')
+    if name == '':
+        break
+    elif name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
+        
+        
